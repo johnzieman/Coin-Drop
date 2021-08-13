7aa66a60c6cada16aeb452fb898fe609219462cb
johnzieman <j.g.zokirov@gmail.com>
2021-08-14 04:09:55 +0500

    BottomNavigation added

--- app/src/main/java/ziemapp/johnzieman/coindrop/MainActivity.kt
+++ app/src/main/java/ziemapp/johnzieman/coindrop/MainActivity.kt
@@ -2,10 +2,22 @@ package ziemapp.johnzieman.coindrop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.containerFragment)
+        bottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNavigationView.setupWithNavController(navController)
+
     }
 }
